625d25b1626886ef0853ccdb67e1aef83778aed1
silvia pintus <silviapintus.93@gmail.com>
2021-06-15 20:09:01 +0100

    getting data from api

--- app.js
+++ app.js
@@ -7,10 +7,18 @@ const app = express();
 
 app.get("/", function(req, res){
 
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=448385fead47b54d9a6b8ef9e045b479";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=448385fead47b54d9a6b8ef9e045b479";
 
   https.get(url, function(response){
-    console.log(response);
+    console.log(response.statusCode);
+
+    response.on("data", function(data){
+      const weatherApp = JSON.parse(data);
+      const temp = weatherApp.main.temp;
+      const description = weatherApp.weather[0].description;
+      console.log(temp);
+      console.log(description);
+    })
   });
 
   res.send("Testing");
