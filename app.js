cd3cf80c6881d6bd42e44a1ff63958ec118c44b1
silvia pintus <silviapintus.93@gmail.com>
2021-06-18 15:06:53 +0100

    app logic done

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
-
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 app.get("/", function(req, res){
+  res.sendFile(__dirname + "/index.html")
+})
 
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=448385fead47b54d9a6b8ef9e045b479";
+app.post("/", function(req, res){
+
+  const apiKey = "448385fead47b54d9a6b8ef9e045b479";
+  const query = req.body.cityName;
+  const unit = "metric";
+
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query +"&units="+ unit +"&appid=" + apiKey;
 
   https.get(url, function(response){
     console.log(response.statusCode);
@@ -19,7 +28,7 @@ app.get("/", function(req, res){
       const icon = weatherApp.weather[0].icon;
       const imgUrl = " http://openweathermap.org/img/wn/" + icon + "@2x.png"
 
-      res.write("<h1>The temperature in London is: " + temp + "</h1>");
+      res.write("<h1>The temperature in " + query + " is: " + temp + " degrees</h1>");
       res.write("<h3>" + description + "</h3>");
       res.write("<img src =" + imgUrl + ">");
       res.send();
